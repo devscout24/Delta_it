292c33f7a6b2cbd9549e16ab4d300b90a2c969da
Konikakobir28 <konikakobir121@gmail.com>
2025-11-16 00:03:43 +0600

    Company Payment API

--- app/Models/CompanyPayment.php
+++ app/Models/CompanyPayment.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyPayment extends Model
 {
-    //
+    protected $fillable = [
+        'company_id',
+        'year',
+        'month',
+        'value_non_vat',
+        'value_vat',
+        'printings_non_vat',
+        'printings_vat',
+        'total_vat',
+        'total_amount',
+        'status',
+    ];
+
+    /**
+     * Relationship: A company payment belongs to a company
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
